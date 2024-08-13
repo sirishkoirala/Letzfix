88608458193006b657547d595830a5f6ee2e0ea4
Sirshkoirala <seris.koerala@gmail.com>
2024-08-13 08:34:24 +0545

    feat: implement static API call

--- src/app/Pages/Homepage.tsx
+++ src/app/Pages/Homepage.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import HeroHomePage from "../components/HeroHomePage";
@@ -9,11 +9,28 @@ import { Device } from "../types/deviceTypes";
 const Homepage: React.FC = () => {
    const [devices, setDevices] = useState<Device[]>([]);
 
+   const fetchDevices = async () => {
+      try {
+         const deviceData: Device[] = [];
+
+         for (let i = 0; i <= 4; i++) {
+            const response = await fetch(`http://localhost:3000/${i}`);
+            if (!response.ok) {
+               throw new Error(`HTTP error! status: ${response.status} for /${i}`);
+            }
+            const data: Device = await response.json();
+            deviceData.push(data);
+         }
+
+         setDevices(deviceData);
+      } catch (err) {
+         console.error(err instanceof Error ? err.message : "An unexpected error occurred");
+      }
+   };
+   console.log(devices);
+
    useEffect(() => {
-      fetch("./data/devices.json")
-         .then((response) => response.json())
-         .then((data: Device[]) => setDevices(data))
-         .catch((error) => console.error("Error fetching devices:", error));
+      fetchDevices();
    }, []);
 
    return (
